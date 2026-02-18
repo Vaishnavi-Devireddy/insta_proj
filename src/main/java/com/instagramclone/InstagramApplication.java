8857a56715a583dfeae85cc70c48c6694bb68127
Vaishnavi Devireddy <vaishdevireddy18@gmail.com>
2026-02-18 12:07:55 +0530

    Added pom.xml

---
+++ src/main/java/com/instagramclone/InstagramApplication.java
@@ -0,0 +1,8 @@
+package com.instagramclone;
+
+public class InstagramApplication {
+    public static void main(String[] args) {
+        System.out.println("Instagram Clone Backend Started");
+    }
+}
+
